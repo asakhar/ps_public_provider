f6cca1685bc6d6e8e52ecfa05d5b54b668f9faa3
asakhar <a@sakhar.ru>
2020-11-24 17:47:28 +0300

    fix

--- t1.9/solution.php
+++ t1.9/solution.php
@@ -2,13 +2,17 @@
 function inverseConcat(...$params) {
   $result = "";
   foreach ($params as $param) {
-    if(!is_string($param))
+    if (!is_string($param)) {
       throw new TypeError;
-    $result = mb_convert_case($param, MB_CASE_LOWER, "UTF-8").(empty($result)?"":" ").$result;
+    }
+
+    $result = mb_convert_case($param, MB_CASE_LOWER, "UTF-8") . (empty($result) ? "" : " ") . $result;
   }
-  if(empty($result))
+  if (empty($result)) {
     return "";
-  return mb_convert_case(mb_substr($result,0,1), MB_CASE_UPPER, "UTF-8").mb_substr($result,1);
+  }
+
+  return mb_convert_case(mb_substr($result, 0, 1), MB_CASE_UPPER, "UTF-8") . mb_substr($result, 1);
 
   // $result = implode(" ", array_reverse($params));
   // return mb_convert_case(mb_substr($result, 0, 1), MB_CASE_TITLE, "UTF-8").mb_substr($result, 1);
@@ -17,21 +21,29 @@ function concatRegister(...$params) {
   $flicker = 0;
   $result = "";
   foreach ($params as $param) {
-    if(!is_string($param))
+    if (!is_string($param)) {
       throw new TypeError;
-    $result .= (empty($result)?"":"-").mb_convert_case($param, $flicker ? MB_CASE_UPPER : MB_CASE_LOWER, "UTF-8");
+    }
+
+    $result .= (empty($result) ? "" : "-") . mb_convert_case($param, $flicker ? MB_CASE_UPPER : MB_CASE_LOWER, "UTF-8");
     $flicker = !$flicker;
   }
   return $result;
 }
 function removeSymbols(...$params) {
-  $flicker = 1;
+  $flicker = 0;
   $result = "";
   foreach ($params as $param) {
-    if(!is_string($param))
+    if (!is_string($param)) {
       throw new TypeError;
-    $result .= (empty($result)?"":" ").mb_ereg_replace($flicker?"[е]":"[т]", "", $param);
+    }
+
+    $result .= (empty($result) ? "" : " ") . mb_ereg_replace($flicker ? "[е]" : "[т]", "", $param);
     $flicker = !$flicker;
   }
   return $result;
+}
+
+if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
+  echo removeSymbols("Первый", "Второй", "Третий", "Четвертый", "Пятый");
 }
